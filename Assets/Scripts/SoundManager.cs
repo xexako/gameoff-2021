db7739f29f125fe760767ed91902708db8f465c9
JBG <jesubg_jademan@hotmail.com>
2021-12-01 01:14:52 +0100

    RELEASE, more levels, music, boundaries

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     static SoundManager soundManager;
-
+   
+    public AudioSource menu;
     public AudioSource loop;
     public AudioSource button;
     public AudioSource victory;
@@ -19,15 +21,28 @@ public class SoundManager : MonoBehaviour
         }
         else
         {
-            soundManager = this;
-            DontDestroyOnLoad(gameObject);
+            InitManager();
         }
+    }
+
+    public void InitManager() {
+        soundManager = this;
+        DontDestroyOnLoad(gameObject);
+        CheckCurrentTheme();
+    }
+
+    private void Update()
+    {
+        CheckCurrentTheme();
     }
 
     public void PlayEffect(string idEffect)
     {
         switch (idEffect)
         {
+            case "MENU":
+                menu.Play();
+                break;
             case "LOOP":
                 loop.Play();
                 break;
@@ -43,6 +58,25 @@ public class SoundManager : MonoBehaviour
             default:
                 Debug.Log("Error");
                 break;
+        }
+    }
+
+    private void CheckCurrentTheme() {
+        if (SceneManager.GetActiveScene().name == "MainMenuScene")
+        {
+            if (!menu.isPlaying)
+            {
+                loop.Stop();
+                menu.Play();
+            }
+        }
+        else
+        {
+            if (!loop.isPlaying)
+            {
+                loop.Play();
+                menu.Stop();
+            }
         }
     }
 }
